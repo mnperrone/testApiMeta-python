c64187dedbb95c9938511d28703fea3e52dd4d11
mperrone <mperrone@sugit.com.ar>
2024-08-19 13:37:21 -0300

    webhook prueba

--- app.py
+++ app.py
@@ -78,10 +78,20 @@ def verificar_token(req):
         return jsonify({'error':'token invalido'}),401
 
 def recibir_mensajes(req):
-    req = request.get_json()
-    agregar_mensajes_log(req)
+    try:
+        req = request.get_json()
+        entry = req['entry'][0]
+        changes = entry['changes'][0]
+        value = changes['value']
+        objeto_mensaje = value['messages']
 
-    return jsonify({'message':'EVENT_RECEIVED'})
+        agregar_mensajes_log(objeto_mensaje)
+
+        return jsonify({'message':'EVENT_RECEIVED'})
+    except Exception as e:
+        return jsonify({'message':'EVENT_RECEIVED'})    
+
+    
     
 
 if __name__ == '__main__':
