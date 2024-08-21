6ffb2bc127d20ed52ee0e06d3c0259b94c30a37d
mperrone <mperrone@sugit.com.ar>
2024-08-21 11:21:41 -0300

    app del profe

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timezone
+from datetime import datetime
 import http.client
 import json
 
 app = Flask(__name__)
 
-# Configuracion de la base de datos SQLite
+#Configuracion de la base de datos SQLITE
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///metapython.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db =SQLAlchemy(app)
@@ -14,47 +14,41 @@ db =SQLAlchemy(app)
 #Modelo de la tabla log
 class Log(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    fecha_y_hora = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    fecha_y_hora = db.Column(db.DateTime, default=datetime.utcnow)
     texto = db.Column(db.TEXT)
-    
-    #constructor de la clase Log
-    def __init__(self, texto):
-        self.texto = texto
 
 #Crear la tabla si no existe
 with app.app_context():
     db.create_all()
 
-
-#Funcion para ordenar registros por fecha y hora
+#Funcion para ordenar los registros por fecha y hora
 def ordenar_por_fecha_y_hora(registros):
-    return sorted(registros, key=lambda x: x.fecha_y_hora, reverse=True)
+    return sorted(registros, key=lambda x: x.fecha_y_hora,reverse=True)
 
 @app.route('/')
 def index():
-    # Obtener todos los registros de la base de datos
+    #obtener todos los registros ed la base de datos
     registros = Log.query.all()
     registros_ordenados = ordenar_por_fecha_y_hora(registros)
     return render_template('index.html',registros=registros_ordenados)
 
 mensajes_log = []
 
-#Función para agregar mensajes y guardar en la base de datos
+#Funcion para agregar mensajes y guardar en la base de datos
 def agregar_mensajes_log(texto):
-    texto = str(texto)
     mensajes_log.append(texto)
+
     #Guardar el mensaje en la base de datos
     nuevo_registro = Log(texto=texto)
     db.session.add(nuevo_registro)
     db.session.commit()
 
-#TOKEN DE VERIFICACION PARA LA CONFIGURACION
-TOKEN_MPERRO = "MPERRO"
+#Token de verificacion para la configuracion
+TOKEN_ANDERCODE = "ANDERCODE"
 
 @app.route('/webhook', methods=['GET','POST'])
-
 def webhook():
-    if request.method== 'GET':
+    if request.method == 'GET':
         challenge = verificar_token(request)
         return challenge
     elif request.method == 'POST':
@@ -65,16 +59,15 @@ def verificar_token(req):
     token = req.args.get('hub.verify_token')
     challenge = req.args.get('hub.challenge')
 
-    if challenge and token == TOKEN_MPERRO:
+    if challenge and token == TOKEN_ANDERCODE:
         return challenge
     else:
-        return jsonify({'error':'token invalido'}),401
- 
+        return jsonify({'error':'Token Invalido'}),401
+
 def recibir_mensajes(req):
     try:
         req = request.get_json()
-        agregar_mensajes_log("Mensaje recibido: " + str(req))  # Registro de depuración
-        entry = req['entry'][0]
+        entry =req['entry'][0]
         changes = entry['changes'][0]
         value = changes['value']
         objeto_mensaje = value['messages']
@@ -86,7 +79,7 @@ def recibir_mensajes(req):
                 tipo = messages["type"]
 
                 #Guardar Log en la BD
-                agregar_mensajes_log(messages)
+                agregar_mensajes_log(json.dumps(messages))
 
                 if tipo == "interactive":
                     tipo_interactivo = messages["interactive"]["type"]
@@ -96,56 +89,283 @@ def recibir_mensajes(req):
                         numero = messages["from"]
 
                         enviar_mensajes_whatsapp(text,numero)
-
+                    
                     elif tipo_interactivo == "list_reply":
                         text = messages["interactive"]["list_reply"]["id"]
                         numero = messages["from"]
 
-                        enviar_mensajes_whatsapp(text,numero)    
+                        enviar_mensajes_whatsapp(text,numero)
 
                 if "text" in messages:
                     text = messages["text"]["body"]
                     numero = messages["from"]
-                    #agregar_mensajes_log("Texto del mensaje: " + text)  # Registro de depuración
-                    #agregar_mensajes_log("Número de teléfono: " + numero)  # Registro de depuración
+
                     enviar_mensajes_whatsapp(text,numero)
 
                     #Guardar Log en la BD
-                    agregar_mensajes_log(messages)
+                    agregar_mensajes_log(json.dumps(messages))
 
         return jsonify({'message':'EVENT_RECEIVED'})
     except Exception as e:
-        agregar_mensajes_log("Error: " + str(e))  # Registro de depuración
-        return jsonify({'message':'EVENT_RECEIVED'})     
+        return jsonify({'message':'EVENT_RECEIVED'})
 
 def enviar_mensajes_whatsapp(texto,number):
     texto = texto.lower()
-    if "hola" in texto:
-        data = {
-                    "messaging_product": "whatsapp",    
-                    "recipient_type": "individual",
-                    "to": number,
-                    "type": "text",
-                    "text": {
-                        "preview_url": False,
-                        "body": "Hola Gatienzoo"
-                    }
-                }
-    else:
-                data = {
-                    "messaging_product": "whatsapp",    
-                    "recipient_type": "individual",
-                    "to": number,
-                    "type": "text",
-                    "text": {
-                        "preview_url": False,
-                        "body": "Primero se saluda, buen dia capo, no?"
-                    }
-                }
-    agregar_mensajes_log("Enviando mensaje de respuesta: " + str(data))  # Registro de depuración
 
-    #Convertir el diccionario a formato json
-    data = json.dumps(data)
+    if "hola" in texto:
+        data={
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "🚀 Hola, ¿Cómo estás? Bienvenido."
+            }
+        }
+    elif "1" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            }
+        }
+    elif "2" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "to": number,
+            "type": "location",
+            "location": {
+                "latitude": "-12.067158831865067",
+                "longitude": "-77.03377940839486",
+                "name": "Estadio Nacional del Perú",
+                "address": "Cercado de Lima"
+            }
+        }
+    elif "3" in texto:
+        data={
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "document",
+            "document": {
+                    "link": "https://www.turnerlibros.com/wp-content/uploads/2021/02/ejemplo.pdf",
+                    "caption": "Temario del Curso #001"
+                }
+            }
+    elif "4" in texto:
+        data={
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "audio",
+            "audio": {
+                "link": "https://filesamples.com/samples/audio/mp3/sample1.mp3"
+            }
+        }
+    elif "5" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "to": number,
+            "text": {
+                "preview_url": True,
+                "body": "Introduccion al curso! https://youtu.be/6ULOE2tGlBM"
+            }
+        }
+    elif "6" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "🤝 En breve me pondre en contacto contigo. 🤓"
+            }
+        }
+    elif "7" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "📅 Horario de Atención : Lunes a Viernes. \n🕜 Horario : 9:00 am a 5:00 pm 🤓"
+            }
+        }
+    elif "0" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "🚀 Hola, visita mi web anderson-bastidas.com para más información.\n \n📌Por favor, ingresa un número #️⃣ para recibir información.\n \n1️⃣. Información del Curso. ❔\n2️⃣. Ubicación del local. 📍\n3️⃣. Enviar temario en PDF. 📄\n4️⃣. Audio explicando curso. 🎧\n5️⃣. Video de Introducción. ⏯️\n6️⃣. Hablar con AnderCode. 🙋‍♂️\n7️⃣. Horario de Atención. 🕜 \n0️⃣. Regresar al Menú. 🕜"
+            }
+        }
+    elif "boton" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "interactive",
+            "interactive":{
+                "type":"button",
+                "body": {
+                    "text": "¿Confirmas tu registro?"
+                },
+                "footer": {
+                    "text": "Selecciona una de las opciones"
+                },
+                "action": {
+                    "buttons":[
+                        {
+                            "type": "reply",
+                            "reply":{
+                                "id":"btnsi",
+                                "title":"Si"
+                            }
+                        },{
+                            "type": "reply",
+                            "reply":{
+                                "id":"btnno",
+                                "title":"No"
+                            }
+                        },{
+                            "type": "reply",
+                            "reply":{
+                                "id":"btntalvez",
+                                "title":"Tal Vez"
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+    elif "btnsi" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "Muchas Gracias por Aceptar."
+            }
+        }
+    elif "btnno" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "Es una Lastima."
+            }
+        }
+    elif "btntalvez" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "Estare a la espera."
+            }
+        }
+    elif "lista" in texto:
+        data ={
+            "messaging_product": "whatsapp",
+            "to": number,
+            "type": "interactive",
+            "interactive":{
+                "type" : "list",
+                "body": {
+                    "text": "Selecciona Alguna Opción"
+                },
+                "footer": {
+                    "text": "Selecciona una de las opciones para poder ayudarte"
+                },
+                "action":{
+                    "button":"Ver Opciones",
+                    "sections":[
+                        {
+                            "title":"Compra y Venta",
+                            "rows":[
+                                {
+                                    "id":"btncompra",
+                                    "title" : "Comprar",
+                                    "description": "Compra los mejores articulos de tecnologia"
+                                },
+                                {
+                                    "id":"btnvender",
+                                    "title" : "Vender",
+                                    "description": "Vende lo que ya no estes usando"
+                                }
+                            ]
+                        },{
+                            "title":"Distribución y Entrega",
+                            "rows":[
+                                {
+                                    "id":"btndireccion",
+                                    "title" : "Local",
+                                    "description": "Puedes visitar nuestro local."
+                                },
+                                {
+                                    "id":"btnentrega",
+                                    "title" : "Entrega",
+                                    "description": "La entrega se realiza todos los dias."
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    elif "btncompra" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "Los mejos articulos top en ofertas."
+            }
+        }
+    elif "btnvender" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "Excelente elección."
+            }
+        }
+    else:
+        data={
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "🚀 Hola, visita mi web anderson-bastidas.com para más información.\n \n📌Por favor, ingresa un número #️⃣ para recibir información.\n \n1️⃣. Información del Curso. ❔\n2️⃣. Ubicación del local. 📍\n3️⃣. Enviar temario en PDF. 📄\n4️⃣. Audio explicando curso. 🎧\n5️⃣. Video de Introducción. ⏯️\n6️⃣. Hablar con AnderCode. 🙋‍♂️\n7️⃣. Horario de Atención. 🕜 \n0️⃣. Regresar al Menú. 🕜"
+            }
+        }
+
+    #Convertir el diccionaria a formato JSON
+    data=json.dumps(data)
 
     headers = {
         "Content-Type" : "application/json",
@@ -155,19 +375,13 @@ def enviar_mensajes_whatsapp(texto,number):
     connection = http.client.HTTPSConnection("graph.facebook.com")
 
     try:
-        connection.request("POST","/v20.0/368298853039307/messages", data, headers)
-        conn = str(connection.request("POST","/v20.0/368298853039307/messages", data, headers))
+        connection.request("POST","/v18.0/117721278011867/messages", data, headers)
         response = connection.getresponse()
-        if response.status == 200:
-            print("Mensaje enviado exitosamente")
-            agregar_mensajes_log(conn)
-        else:
-            agregar_mensajes_log(conn)
-            print("Error al enviar mensaje:", response.status, response.reason)
+        print(response.status, response.reason)
     except Exception as e:
-        agregar_mensajes_log(e)
+        agregar_mensajes_log(json.dumps(e))
     finally:
         connection.close()
 
-if __name__ == '__main__':
+if __name__=='__main__':
     app.run(host='0.0.0.0',port=80,debug=True)
