dfdea06ed83727365cfb454954d68ca11c9b4334
mperrone <mperrone@sugit.com.ar>
2024-08-28 16:08:05 -0300

    proyectos

--- app.py
+++ app.py
@@ -169,9 +169,22 @@ def enviar_mensajes_whatsapp(texto, number):
             "type": "document",
             "document": {
                 "link": "https://mln.ar/descargas/1154-D-2023-IAPI-XXI.pdf",
+                "link": "https://mln.ar/descargas/0988-D-2023-ELMA.pdf",
                 "caption": "IAPI XXI C.E."
             }
-        } 
+        }
+    elif "4" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "document",
+            "document": {
+                "link": "https://mln.ar/descargas/1154-D-2023-IAPI-XXI.pdf",
+                "link": "https://mln.ar/descargas/0988-D-2023-ELMA.pdf",
+                "caption": "IAPI XXI C.E."
+            }
+        }     
     else:
         data = {
             "messaging_product": "whatsapp",
@@ -180,7 +193,7 @@ def enviar_mensajes_whatsapp(texto, number):
             "type": "text",
             "text": {
                 "preview_url": False,
-                "body": "🇦🇷 Hola Argentin@, visita la web del movimiento https://mln.ar/ para más información, allí también podrás afiliarte.\n\n📌Por favor, ingresa un número #️⃣ para recibir información.\n\n1️⃣. Información del Proyecto. ❔\n2️⃣. Ubicación de los grupos de militancia en el AMBA. 📍\n3️⃣. Enviar proyecto de ley IAPI XXI. 📄\n4️⃣. Audio explicando el proyecto. 🎧\n5️⃣. Video de Introducción. ⏯️\n6️⃣. Hablar con algún compañero. 🙋‍♂️\n7️⃣. Próximas actividades. 🕜\n0️⃣. Regresar al Menú. 🕜"
+                "body": "🇦🇷 Hola Argentin@, visita la web del movimiento https://mln.ar/ para más información, allí también podrás afiliarte.\n\n📌Por favor, ingresa un número #️⃣ para recibir información.\n\n1️⃣. Información del Proyecto. ❔\n2️⃣. Ubicación de los grupos de militancia en el AMBA. 📍\n3️⃣. Enviar proyecto de ley IAPI XXI C.E. 📄 📍\n4️⃣Enviar proyecto de ley ELMA XXI C.E. 📄\n5️⃣. Audio explicando el proyecto. 🎧\n6️⃣. Video de Introducción. ⏯️\n7️⃣. Hablar con algún compañero. 🙋‍♂️\n8️⃣. Próximas actividades. 🕜\n0️⃣. Regresar al Menú. 🕜"
             }
         }
     
